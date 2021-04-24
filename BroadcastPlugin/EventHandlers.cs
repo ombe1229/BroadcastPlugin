23feb020a6b4d317d4665ad99dac7c749617d44b
Restyled.io <commits@restyled.io>
2021-04-24 14:31:50 +0000

    Restyled by astyle

--- BroadcastPlugin/EventHandlers.cs
+++ BroadcastPlugin/EventHandlers.cs
@@ -6,131 +6,131 @@ using Respawning;
 
 namespace BroadcastPlugin
 {
-    public class EventHandlers
+public class EventHandlers
+{
+    private readonly BroadcastPlugin _pluginInstance;
+    public EventHandlers(BroadcastPlugin pluginInstance) => _pluginInstance = pluginInstance;
+    internal void OnRoundStarting()
     {
-        private readonly BroadcastPlugin _pluginInstance;
-        public EventHandlers(BroadcastPlugin pluginInstance) => _pluginInstance = pluginInstance;
-        internal void OnRoundStarting()
-        {
-            if (!_pluginInstance.Config.IsEnabled) return;
-            Map.Broadcast(10,"<color=green>라운드가 시작되었습니다.</color>");
-            Log.Info("이 서버는 ombe1229의 브로드캐스트 플러그인을 사용 중입니다.");
-        }
+        if (!_pluginInstance.Config.IsEnabled) return;
+        Map.Broadcast(10,"<color=green>라운드가 시작되었습니다.</color>");
+        Log.Info("이 서버는 ombe1229의 브로드캐스트 플러그인을 사용 중입니다.");
+    }
 
-        internal void OnRoundEnding(RoundEndedEventArgs ev)
-        {
-            Map.Broadcast(10,"<color=green>라운드가 종료되었습니다.</color>");
-        }
-        
-        internal void OnAnnouncingDecontamination(AnnouncingDecontaminationEventArgs ev)
-        {
-            switch (ev.Id)
-            {
-                case 0:
-                {
-                    Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>13분</color> 남았습니다.");
-                    break;
-                }
-                case 1:
-                {
-                    Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>10분</color> 남았습니다.");
-                    break;
-                }
-                case 2:
-                {
-                    Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>5분</color> 남았습니다.");
-                    break;
-                }
-                case 3:
-                {
-                    Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>1분</color> 남았습니다.\n저위험군에 남아있는 인원은 신속히 대피하시기 바랍니다.");
-                    break;
-                }
-                case 4:
-                {
-                    Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>30초</color> 남았습니다.\n저위험군에 남아있는 인원은 신속히 대피하시기 바랍니다.");
-                    break;
-                }
-            }
-        }
+    internal void OnRoundEnding(RoundEndedEventArgs ev)
+    {
+        Map.Broadcast(10,"<color=green>라운드가 종료되었습니다.</color>");
+    }
 
-        internal void OnDecontaminating(DecontaminatingEventArgs ev)
+    internal void OnAnnouncingDecontamination(AnnouncingDecontaminationEventArgs ev)
+    {
+        switch (ev.Id)
         {
-            Map.Broadcast(10,"<color=red>저위험군 격리 절차</color>가 시작되었습니다.");
+        case 0:
+        {
+            Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>13분</color> 남았습니다.");
+            break;
         }
-
-        internal void OnRespawningTeam(RespawningTeamEventArgs ev)
+        case 1:
         {
-            if (ev.NextKnownTeam != SpawnableTeamType.ChaosInsurgency) return;
-      
-            foreach (Player player in Player.List)
-            {
-                if (player.Team == Team.CDP || player.Team == Team.CHI) player.Broadcast(10,"<color=army_green>혼돈의 반란</color>이 시설 내에 진입했습니다.");
-            }
+            Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>10분</color> 남았습니다.");
+            break;
         }
-
-        internal void OnAnnouncingNtfEntrance(AnnouncingNtfEntranceEventArgs ev)
+        case 2:
         {
-            Map.Broadcast(10,$"<color=cyan>기동특무부대 {ev.UnitName}-{ev.UnitNumber}</color>이 시설 내에 진입했습니다.\n재격리 대기 중인 <color=red>SCP</color>개체는 <color=red>{ev.ScpsLeft}</color>마리입니다.");
+            Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>5분</color> 남았습니다.");
+            break;
         }
-
-        internal void OnDied(DiedEventArgs ev)
+        case 3:
         {
-            Team playerTeam = ev.Target.Team;
-            int teamLeft = 0;
-            Player lastplayer = null;
-            foreach (Player player in Player.List)
-            {
-                if (!player.IsEnemy(playerTeam) && player != ev.Target)
-                {
-                    teamLeft++;
-                    lastplayer = player;
-                }
-            }
-            if (teamLeft == 1)
-            {
-                lastplayer?.Broadcast(10, "당신이 현재 진영의 <color=red>마지막 생존자</color>입니다!");
-            }
+            Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>1분</color> 남았습니다.\n저위험군에 남아있는 인원은 신속히 대피하시기 바랍니다.");
+            break;
         }
-
-        internal void OnEnteringFemurBreaker(EnteringFemurBreakerEventArgs ev)
+        case 4:
         {
-            Map.Broadcast(10,$"<color=red>{ev.Player.Nickname}</color>님이 대퇴골 분쇄기에 진입했습니다.");
+            Map.Broadcast(10,"<color=red>저위험군 격리 절차</color> 실행까지 <color=red>30초</color> 남았습니다.\n저위험군에 남아있는 인원은 신속히 대피하시기 바랍니다.");
+            break;
         }
-
-        internal void OnGeneratorActivated(GeneratorActivatedEventArgs ev)
-        {
-            if (Generator079.mainGenerator.forcedOvercharge)
-                return;
-            
-            int cur = Generator079.mainGenerator.NetworktotalVoltage + 1;
-            if (cur != 5)
-            {
-                Map.Broadcast(10,$"발전기 <color=red>5</color>개중 <color=red>{cur}</color>개가 작동되었습니다.");
-            }
-            else
-            {
-                Map.Broadcast(10,"발전기 <color=red>5</color>개가 모두 작동되었습니다.");
-            }
-        }
-
-        internal void OnWarheadStarting(StartingEventArgs ev)
-        {
-            Map.Broadcast(10,"<color=red>알파 탄두</color> 폭파 절차가 발동되었습니다.\n남아있는 인원은 신속히 대피하시기 바랍니다.");
-        }
-
-        internal void OnWarheadStopping(StoppingEventArgs ev)
-        {
-            Map.Broadcast(10,"<color=red>폭파 절차</color>가 취소되었습니다. 시스템을 재가동합니다.");
-        }
-
-        internal void OnSpawning(SpawningEventArgs ev)
-        {
-            if (ev.Player.Team != Team.SCP) return;
-    
-            IEnumerable<Player> scpList = Player.Get(Team.SCP).ToList();
-            
-            ev.Player.Broadcast(10,$"이번 라운드의 <color=red>SCP</color>는 <color=red>{scpList.Count()}</color>마리입니다.\n{string.Join(" |", scpList.Select(player => player.Role))}");
         }
     }
+
+    internal void OnDecontaminating(DecontaminatingEventArgs ev)
+    {
+        Map.Broadcast(10,"<color=red>저위험군 격리 절차</color>가 시작되었습니다.");
+    }
+
+    internal void OnRespawningTeam(RespawningTeamEventArgs ev)
+    {
+        if (ev.NextKnownTeam != SpawnableTeamType.ChaosInsurgency) return;
+
+        foreach (Player player in Player.List)
+        {
+            if (player.Team == Team.CDP || player.Team == Team.CHI) player.Broadcast(10,"<color=army_green>혼돈의 반란</color>이 시설 내에 진입했습니다.");
+        }
+    }
+
+    internal void OnAnnouncingNtfEntrance(AnnouncingNtfEntranceEventArgs ev)
+    {
+        Map.Broadcast(10,$"<color=cyan>기동특무부대 {ev.UnitName}-{ev.UnitNumber}</color>이 시설 내에 진입했습니다.\n재격리 대기 중인 <color=red>SCP</color>개체는 <color=red>{ev.ScpsLeft}</color>마리입니다.");
+    }
+
+    internal void OnDied(DiedEventArgs ev)
+    {
+        Team playerTeam = ev.Target.Team;
+        int teamLeft = 0;
+        Player lastplayer = null;
+        foreach (Player player in Player.List)
+        {
+            if (!player.IsEnemy(playerTeam) && player != ev.Target)
+            {
+                teamLeft++;
+                lastplayer = player;
+            }
+        }
+        if (teamLeft == 1)
+        {
+            lastplayer?.Broadcast(10, "당신이 현재 진영의 <color=red>마지막 생존자</color>입니다!");
+        }
+    }
+
+    internal void OnEnteringFemurBreaker(EnteringFemurBreakerEventArgs ev)
+    {
+        Map.Broadcast(10,$"<color=red>{ev.Player.Nickname}</color>님이 대퇴골 분쇄기에 진입했습니다.");
+    }
+
+    internal void OnGeneratorActivated(GeneratorActivatedEventArgs ev)
+    {
+        if (Generator079.mainGenerator.forcedOvercharge)
+            return;
+
+        int cur = Generator079.mainGenerator.NetworktotalVoltage + 1;
+        if (cur != 5)
+        {
+            Map.Broadcast(10,$"발전기 <color=red>5</color>개중 <color=red>{cur}</color>개가 작동되었습니다.");
+        }
+        else
+        {
+            Map.Broadcast(10,"발전기 <color=red>5</color>개가 모두 작동되었습니다.");
+        }
+    }
+
+    internal void OnWarheadStarting(StartingEventArgs ev)
+    {
+        Map.Broadcast(10,"<color=red>알파 탄두</color> 폭파 절차가 발동되었습니다.\n남아있는 인원은 신속히 대피하시기 바랍니다.");
+    }
+
+    internal void OnWarheadStopping(StoppingEventArgs ev)
+    {
+        Map.Broadcast(10,"<color=red>폭파 절차</color>가 취소되었습니다. 시스템을 재가동합니다.");
+    }
+
+    internal void OnSpawning(SpawningEventArgs ev)
+    {
+        if (ev.Player.Team != Team.SCP) return;
+
+        IEnumerable<Player> scpList = Player.Get(Team.SCP).ToList();
+
+        ev.Player.Broadcast(10,$"이번 라운드의 <color=red>SCP</color>는 <color=red>{scpList.Count()}</color>마리입니다.\n{string.Join(" |", scpList.Select(player => player.Role))}");
+    }
+}
 }
--- BroadcastPlugin/Plugin.cs
+++ BroadcastPlugin/Plugin.cs
@@ -7,59 +7,62 @@ using Features = Exiled.API.Features;
 
 namespace BroadcastPlugin
 {
-    public class BroadcastPlugin : Features.Plugin<Configs>
-    {
-        public EventHandlers EventHandlers { get; private set; }
-
-        public void LoadEvents()
-        {
-            ServerEvents.RoundStarted += EventHandlers.OnRoundStarting;
-            ServerEvents.RoundEnded += EventHandlers.OnRoundEnding;
-            MapEvents.AnnouncingDecontamination += EventHandlers.OnAnnouncingDecontamination;
-            MapEvents.Decontaminating += EventHandlers.OnDecontaminating;
-            ServerEvents.RespawningTeam += EventHandlers.OnRespawningTeam;
-            MapEvents.AnnouncingNtfEntrance += EventHandlers.OnAnnouncingNtfEntrance;
-            PlayerEvents.Died += EventHandlers.OnDied;
-            PlayerEvents.EnteringFemurBreaker += EventHandlers.OnEnteringFemurBreaker;
-            MapEvents.GeneratorActivated += EventHandlers.OnGeneratorActivated;
-            WarheadEvents.Starting += EventHandlers.OnWarheadStarting;
-            WarheadEvents.Stopping += EventHandlers.OnWarheadStopping;
-            PlayerEvents.Spawning += EventHandlers.OnSpawning;
-        }
-
-        public override void OnEnabled()
-        {
-            if (!Config.IsEnabled) return;
-           
-            base.OnEnabled();
-            
-            EventHandlers = new EventHandlers(this);
-            LoadEvents();
-            Log.Info("브로드캐스트 플러그인 활성화");
-        }
-        
-        public override void OnDisabled()
-        {
-            base.OnDisabled();
-            
-            ServerEvents.RoundStarted -= EventHandlers.OnRoundStarting;
-            ServerEvents.RoundEnded -= EventHandlers.OnRoundEnding;
-            MapEvents.AnnouncingDecontamination -= EventHandlers.OnAnnouncingDecontamination;
-            MapEvents.Decontaminating -= EventHandlers.OnDecontaminating;
-            ServerEvents.RespawningTeam -= EventHandlers.OnRespawningTeam;
-            MapEvents.AnnouncingNtfEntrance -= EventHandlers.OnAnnouncingNtfEntrance;
-            PlayerEvents.Died -= EventHandlers.OnDied;
-            PlayerEvents.EnteringFemurBreaker -= EventHandlers.OnEnteringFemurBreaker;
-            MapEvents.GeneratorActivated -= EventHandlers.OnGeneratorActivated;
-            WarheadEvents.Starting -= EventHandlers.OnWarheadStarting;
-            WarheadEvents.Stopping -= EventHandlers.OnWarheadStopping;
-            PlayerEvents.Spawning -= EventHandlers.OnSpawning;
-            EventHandlers = null;
-        }
-
-        public override void OnReloaded()
-        {
-            
-        }
+public class BroadcastPlugin : Features.Plugin<Configs>
+{
+    public EventHandlers EventHandlers {
+        get;
+        private set;
     }
+
+    public void LoadEvents()
+    {
+        ServerEvents.RoundStarted += EventHandlers.OnRoundStarting;
+        ServerEvents.RoundEnded += EventHandlers.OnRoundEnding;
+        MapEvents.AnnouncingDecontamination += EventHandlers.OnAnnouncingDecontamination;
+        MapEvents.Decontaminating += EventHandlers.OnDecontaminating;
+        ServerEvents.RespawningTeam += EventHandlers.OnRespawningTeam;
+        MapEvents.AnnouncingNtfEntrance += EventHandlers.OnAnnouncingNtfEntrance;
+        PlayerEvents.Died += EventHandlers.OnDied;
+        PlayerEvents.EnteringFemurBreaker += EventHandlers.OnEnteringFemurBreaker;
+        MapEvents.GeneratorActivated += EventHandlers.OnGeneratorActivated;
+        WarheadEvents.Starting += EventHandlers.OnWarheadStarting;
+        WarheadEvents.Stopping += EventHandlers.OnWarheadStopping;
+        PlayerEvents.Spawning += EventHandlers.OnSpawning;
+    }
+
+    public override void OnEnabled()
+    {
+        if (!Config.IsEnabled) return;
+
+        base.OnEnabled();
+
+        EventHandlers = new EventHandlers(this);
+        LoadEvents();
+        Log.Info("브로드캐스트 플러그인 활성화");
+    }
+
+    public override void OnDisabled()
+    {
+        base.OnDisabled();
+
+        ServerEvents.RoundStarted -= EventHandlers.OnRoundStarting;
+        ServerEvents.RoundEnded -= EventHandlers.OnRoundEnding;
+        MapEvents.AnnouncingDecontamination -= EventHandlers.OnAnnouncingDecontamination;
+        MapEvents.Decontaminating -= EventHandlers.OnDecontaminating;
+        ServerEvents.RespawningTeam -= EventHandlers.OnRespawningTeam;
+        MapEvents.AnnouncingNtfEntrance -= EventHandlers.OnAnnouncingNtfEntrance;
+        PlayerEvents.Died -= EventHandlers.OnDied;
+        PlayerEvents.EnteringFemurBreaker -= EventHandlers.OnEnteringFemurBreaker;
+        MapEvents.GeneratorActivated -= EventHandlers.OnGeneratorActivated;
+        WarheadEvents.Starting -= EventHandlers.OnWarheadStarting;
+        WarheadEvents.Stopping -= EventHandlers.OnWarheadStopping;
+        PlayerEvents.Spawning -= EventHandlers.OnSpawning;
+        EventHandlers = null;
+    }
+
+    public override void OnReloaded()
+    {
+
+    }
+}
 }
