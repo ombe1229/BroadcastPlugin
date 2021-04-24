0b441fca906c5edd0af19a9395cd921fefa52832
green1052 <cydyellowgreen@gmail.com>
2021-04-25 00:19:47 +0900

    1

--- BroadcastPlugin/Plugin.cs
+++ BroadcastPlugin/Plugin.cs
@@ -1,4 +1,5 @@
-﻿using Log = Exiled.API.Features.Log;
+﻿using Exiled.API.Enums;
+using Log = Exiled.API.Features.Log;
 using ServerEvents = Exiled.Events.Handlers.Server;
 using PlayerEvents = Exiled.Events.Handlers.Player;
 using MapEvents = Exiled.Events.Handlers.Map;
@@ -11,6 +12,14 @@ namespace BroadcastPlugin
     {
         public EventHandlers EventHandlers { get; private set; }
 
+        public override string Name { get; } = "BroadcastPlugin";
+
+        public override string Prefix { get; } =  "BroadcastPlugin";
+
+        public override string Author { get; } =  "ombe1229";
+
+        public override PluginPriority Priority { get; } = PluginPriority.Default;
+        
         public void LoadEvents()
         {
             ServerEvents.RoundStarted += EventHandlers.OnRoundStarting;
